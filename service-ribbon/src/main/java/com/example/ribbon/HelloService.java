ec2f9b541b07156c7511d4036bdf5543abc65920
wuqionglei <wuqionglei@countrygarden@com.cn>
2019-09-15 13:27:36 +0800

    springcloud笔记

---
+++ service-ribbon/src/main/java/com/example/ribbon/HelloService.java
@@ -0,0 +1,21 @@
+package com.example.ribbon;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author wuqionglei
+ * @date 2019/9/15 12:55
+ */
+
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+}
