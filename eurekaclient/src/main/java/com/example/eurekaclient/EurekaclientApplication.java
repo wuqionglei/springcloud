7623bfd4f0d6ba329fa93776cce3256f52f16556
wuqionglei <wuqionglei@countrygarden@com.cn>
2019-09-15 12:19:31 +0800

    springcloud笔记

--- eurekaclient/src/main/java/com/example/eurekaclient/EurekaclientApplication.java
+++ eurekaclient/src/main/java/com/example/eurekaclient/EurekaclientApplication.java
@@ -4,7 +4,10 @@ package com.example.eurekaclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 //引入RestController注解
+
 import org.springframework.web.bind.annotation.RestController;
 //引入Value注解
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
 public class EurekaclientApplication {
 
