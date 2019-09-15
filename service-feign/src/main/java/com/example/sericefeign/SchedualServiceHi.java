cd15f7dd7d92d3eecacdad46115335f23309a228
wuqionglei <wuqionglei@countrygarden@com.cn>
2019-09-15 14:14:59 +0800

    springcloud笔记

---
+++ service-feign/src/main/java/com/example/sericefeign/SchedualServiceHi.java
@@ -0,0 +1,16 @@
+package com.example.sericefeign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author wuqionglei
+ * @date 2019/9/15 13:42
+ */
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
---
+++ service-feign/src/main/java/com/example/sericefeign/SchedualServiceHiHystric.java
@@ -0,0 +1,15 @@
+package com.example.sericefeign;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wuqionglei
+ * @date 2019/9/15 14:00
+ */
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
